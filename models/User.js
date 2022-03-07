af84074e9147e21ef5370c8877e4d3b17a23c6d8
hannahhan153 <hannah.han@biola.edu>
2022-03-07 17:08:16 -0500

    use sequelize to manage SQL data, use environmental variables to protect sensitive data, created user table using sequelize models, Created all of the server endpoints using RESTful API standards to work with the user model’s data

---
+++ models/User.js
@@ -0,0 +1,56 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+  {
+    // define an id column
+    id: {
+        // use the special Sequelize DataTypes object provide what type of data it is
+        type: DataTypes.INTEGER,
+        // this is the equivalent of SQL's `NOT NULL` option
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type:DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            // at least four characters long
+            len: [4]
+        }
+    }
+  },
+  {
+    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+
+    // pass in our imported sequelize connection (the direct connection to our database)
+    sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
+    timestamps: false,
+    // don't pluralize name of database table
+    freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+    underscored: true,
+    // make it so our model name stays lowercase in the database
+    modelName: 'user'
+  }
+);
+
+module.exports = User;
---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+// collect all API routes and package
+
+const router = require('express').Router();
+
+const userRoutes = require('./user-routes.js');
+
+router.use('/users', userRoutes);
+
+module.exports = router;
